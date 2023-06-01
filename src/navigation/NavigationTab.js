1d9a0b3131c8c599d1ed48bff0a59cb270852430
Juanmanuba <jmnbvm@gmail.com>
2023-06-01 10:29:44 -0400

    Added screens

--- src/navigation/NavigationTab.js
+++ src/navigation/NavigationTab.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import Account from '../screens/Account';
+import PokeDex from '../screens/PokeDex';
+import Favorites from '../screens/Favorites';
 
 const Tab = createBottomTabNavigator();
 
@@ -7,7 +11,9 @@ export default function App() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Account" component={Account} />
+      <Tab.Screen name="PokeDex" component={PokeDex} />
+      <Tab.Screen name="Favorites" component={Favorites} />
     </Tab.Navigator>
   );
 }
